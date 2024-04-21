9fdbf1443a789ac55f3fb916e4fae2a2d8ade7e8
Angel <angel.2899pasj@gmail.com>
2024-04-20 21:20:28 -0600

    first commit

---
+++ SignalR/Controllers/BeerController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.Hubs;
+
+namespace SignalR.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BeerController : ControllerBase
+	{
+		private IHubContext<MessageHub> _hubContext;
+
+
+		public BeerController(IHubContext<MessageHub> hubContext)
+		{
+			_hubContext = hubContext;
+		}
+
+	    [HttpGet]
+		public async Task<IActionResult> Send(string Message)
+		{
+			await _hubContext.Clients.All.SendAsync("SendMessage",Message);
+			return Ok();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Guardar(string Nombre)
+		{
+			await _hubContext.Clients.All.SendAsync("SendMessage", Nombre);
+			return Ok();
+
+		}
+	}
+}
---
+++ SignalR/Models/Noti.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.Models
+{
+    public class Noti
+    {
+        public string texto { get; set; }
+        public string imagen { get; set; }
+        public bool visto { get; set; }
+    }
+}
